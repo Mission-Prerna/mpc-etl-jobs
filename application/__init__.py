2a2d67c68fda09cebbf0854d53302c3c868be0ad
Radhay Anand <radhay@samagragovernance.in>
2021-09-05 17:28:01 +0530

    fixed postgres version and e-pathshala route changes

--- application/__init__.py
+++ application/__init__.py
@@ -34,7 +34,7 @@ DATABASE_TABLE = os.getenv('db_table')
 
 api = Flask(__name__)
 
-def db_connect():
+def get_connection():
     """ Connect to the database server
     Args:
         conn_string: database connection string
@@ -195,7 +195,7 @@ def save_volunteer():
             form_response["grade10"],
             form_response["stumno10"]))
     try:
-        connection = db_connect()
+        connection = get_connection()
         cursor = connection.cursor()
         # Store data in main dump table
         sql = """INSERT INTO prerna_saathi_final
@@ -238,83 +238,84 @@ def save_e_pathshala_quiz_data():
         api.logger.error(f'No data in the input payload')
         return "error"
     else:
-        form_response = request_json["data"][0]
-        
+        final_responses = []
+        for form_response in request_json["data"]:
+            if not form_response["today"]:
+                form_response["today"] = str((datetime.now()).date())
+            # Store data in main dump table
+            deviceid = form_response["deviceid"]
+            phoneno = form_response["phnum"]
+            udise = form_response["school"]
+            stuname = form_response["name"]
+            grade = form_response["grade"]
+            quizno = form_response["quiz_value"]
+            subject = "Hindi"
+            quizstatus = "C"
+            if grade == "3":
+                q1 = form_response["ques_3_hindi_1_ans"]
+                q2 = form_response["ques_3_hindi_2_ans"]
+                q3 = form_response["ques_3_hindi_3_ans"]
+                q4 = form_response["ques_3_hindi_4_ans"]
+                q5 = form_response["ques_3_hindi_5_ans"]
+                q6 = form_response["ques_3_maths_1_ans"]
+                q7 = form_response["ques_3_maths_2_ans"]
+                q8 = form_response["ques_3_maths_3_ans"]
+                q9 = form_response["ques_3_maths_4_ans"]
+                q10 = form_response["ques_3_maths_5_ans"]
+            elif grade == "4":
+                q1 = form_response["ques_4_hindi_1_ans"]
+                q2 = form_response["ques_4_hindi_2_ans"]
+                q3 = form_response["ques_4_hindi_3_ans"]
+                q4 = form_response["ques_4_hindi_4_ans"]
+                q5 = form_response["ques_4_hindi_5_ans"]
+                q6 = form_response["ques_4_maths_1_ans"]
+                q7 = form_response["ques_4_maths_2_ans"]
+                q8 = form_response["ques_4_maths_3_ans"]
+                q9 = form_response["ques_4_maths_4_ans"]
+                q10 = form_response["ques_4_maths_5_ans"]
+            elif grade == "5":
+                q1 = form_response["ques_5_hindi_1_ans"]
+                q2 = form_response["ques_5_hindi_2_ans"]
+                q3 = form_response["ques_5_hindi_3_ans"]
+                q4 = form_response["ques_5_hindi_4_ans"]
+                q5 = form_response["ques_5_hindi_5_ans"]
+                q6 = form_response["ques_5_maths_1_ans"]
+                q7 = form_response["ques_5_maths_2_ans"]
+                q8 = form_response["ques_5_maths_3_ans"]
+                q9 = form_response["ques_5_maths_4_ans"]
+                q10 = form_response["ques_5_maths_5_ans"]
+            else:
+                q1 = q2 = q3 = q4 = q5 = q6 = q7 = q8 = q9 = q10 = None
+            totmarks = form_response["total_score"]
+            maxmarks = 10
+            instance_id = form_response["instanceID"]
+            final_responses.append((deviceid,
+                                    phoneno,
+                                    udise,
+                                    stuname,
+                                    grade,
+                                    quizno,
+                                    subject,
+                                    quizstatus,
+                                    q1,
+                                    q2,
+                                    q3,
+                                    q4,
+                                    q5,
+                                    q6,
+                                    q7,
+                                    q8,
+                                    q9,
+                                    q10,
+                                    totmarks,
+                                    maxmarks,
+                                    instance_id))
     try:
-        connection = db_connect()
+        connection = get_connection()
         cursor = connection.cursor()
-        # Store data in main dump table
-        deviceid = form_response["deviceid"]
-        phoneno = form_response["phnum"]
-        udise = form_response["school"]
-        stuname = form_response["name"]
-        grade = form_response["grade"]
-        quizno = form_response["quiz_value"]
-        subject = "Hindi"
-        quizstatus = "C"
-        if grade == "3":
-            q1 = form_response["ques_3_hindi_1_ans"]
-            q2 = form_response["ques_3_hindi_2_ans"]
-            q3 = form_response["ques_3_hindi_3_ans"]
-            q4 = form_response["ques_3_hindi_4_ans"]
-            q5 = form_response["ques_3_hindi_5_ans"]
-            q6 =form_response["ques_3_maths_1_ans"]
-            q7 = form_response["ques_3_maths_2_ans"]
-            q8 = form_response["ques_3_maths_3_ans"]
-            q9 = form_response["ques_3_maths_4_ans"]
-            q10 = form_response["ques_3_maths_5_ans"]
-        elif grade == "4":
-            q1 = form_response["ques_4_hindi_1_ans"]
-            q2 = form_response["ques_4_hindi_2_ans"]
-            q3 = form_response["ques_4_hindi_3_ans"]
-            q4 = form_response["ques_4_hindi_4_ans"]
-            q5 = form_response["ques_4_hindi_5_ans"]
-            q6 =form_response["ques_4_maths_1_ans"]
-            q7 = form_response["ques_4_maths_2_ans"]
-            q8 = form_response["ques_4_maths_3_ans"]
-            q9 = form_response["ques_4_maths_4_ans"]
-            q10 = form_response["ques_4_maths_5_ans"]
-        elif grade == "5":
-            q1 = form_response["ques_5_hindi_1_ans"]
-            q2 = form_response["ques_5_hindi_2_ans"]
-            q3 = form_response["ques_5_hindi_3_ans"]
-            q4 = form_response["ques_5_hindi_4_ans"]
-            q5 = form_response["ques_5_hindi_5_ans"]
-            q6 =form_response["ques_5_maths_1_ans"]
-            q7 = form_response["ques_5_maths_2_ans"]
-            q8 = form_response["ques_5_maths_3_ans"]
-            q9 = form_response["ques_5_maths_4_ans"]
-            q10 = form_response["ques_5_maths_5_ans"]
-        else:
-            q1 = q2 = q3 = q4 = q5 = q6 = q7 = q8 = q9 = q10 = None
-        totmarks = form_response["total_score"]
-        maxmarks = 10
-        instance_id = form_response["instanceID"] 
-
         sql = """INSERT INTO epathshala_quiz_responses1
             (deviceid, phoneno, udise, stuname, grade, quizno, subject, quizstatus, q1, q2, q3,  q4, q5, q6,  q7, q8,  q9, q10, totmarks, maxmarks, instance_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-        cursor.execute(sql, ( 
-            deviceid,
-            phoneno,
-            udise,
-            stuname,
-            grade, 
-            quizno,
-            subject, 
-            quizstatus,
-            q1,
-            q2,
-            q3,
-            q4,
-            q5,
-            q6,
-            q7,
-            q8,
-            q9,
-            q10,
-            totmarks,
-            maxmarks,
-            instance_id ) )
+        execute_values(cursor, sql, final_responses)
         connection.commit()
     except (Exception, psycopg2.Error) as error:
         api.logger.error(f"Error in update operation {error}")
