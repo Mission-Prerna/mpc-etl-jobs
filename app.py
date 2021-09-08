fc47fe64498b42d739e28ed36bc29e1d3190dee0
Radhay Anand <radhay@samagragovernance.in>
2021-09-08 16:06:45 +0530

    added celery configs

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from datetime import datetime
 import os
 import re
 from celery import Celery
+from .utils import get_tuple_from_dict
+from . import constants, queries
 
 # Set up logging
 logger = logging.getLogger()
@@ -39,8 +41,11 @@ DATABASE_TABLE = os.getenv('db_table')
 
 api = Flask(__name__)
 # Celery configuration
-api.config['CELERY_BROKER_URL'] = 'amqp://testing:blody_mary_789@165.232.182.146:5672'
-api.config['CELERY_RESULT_BACKEND'] = 'rpc://testing:blody_mary_789@165.232.182.146:5672'
+api.config['CELERY_BROKER_URL'] = os.getenv('CELERY_BROKER_URL')
+api.config['CELERY_RESULT_BACKEND'] = os.getenv('CELERY_RESULT_BACKEND')
+
+max_retries = os.getenv('max_retries')
+retry_backoff = os.getenv('retry_backoff')
 
 # Initialize Celery
 celery = Celery(api.name, broker=api.config['CELERY_BROKER_URL'])
@@ -72,239 +77,13 @@ def starting_url():
     return jsonify(data)
 
 
-@celery.task
-def add_task_volunteer(data):
+@celery.task(max_retries=max_retries, retry_backoff=retry_backoff, autoretry_for=(Exception, psycopg2.Error))
+def add_task(data, query, param_list):
     try:
         connection = get_connection()
         cursor = connection.cursor()
-        for form_response in data:
-            form_response = form_response[0]
-            if not form_response['today']:
-                form_response['today'] = str((datetime.now()).date())
-            # Store data in main dump table
-            sql = """INSERT INTO prerna_saathi_final
-                        (whatsapp_number, volunteer_name, gender, age, phone_number, school_udise_code, student_opted_number, student_1_name, student_1_grade, student_1_number,student_2_name,  student_2_grade, student_2_number,student_3_name,  student_3_grade, student_3_number,  student_4_name, student_4_grade, student_4_number, student_5_name,student_5_grade, student_5_number, student_6_name, student_6_grade,  student_6_number, student_7_name, student_7_grade, student_7_number, student_8_name, student_8_grade, student_8_number, student_9_name, student_9_grade, student_9_number, student_10_name,student_10_grade, student_10_number, device_id, registration_date, instance_id  ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                        ON CONFLICT (whatsapp_number)
-                        DO UPDATE SET volunteer_name = %s, gender = %s, age = %s, phone_number = %s, school_udise_code = %s, student_opted_number = %s, student_1_name = %s, student_1_grade = %s, student_1_number = %s,student_2_name = %s,  student_2_grade = %s, student_2_number = %s,student_3_name = %s,  student_3_grade = %s, student_3_number = %s,  student_4_name = %s, student_4_grade = %s, student_4_number = %s, student_5_name = %s,student_5_grade = %s, student_5_number = %s, student_6_name = %s, student_6_grade = %s,  student_6_number = %s, student_7_name = %s, student_7_grade = %s, student_7_number = %s, student_8_name = %s, student_8_grade = %s, student_8_number = %s, student_9_name = %s, student_9_grade = %s, student_9_number = %s, student_10_name = %s,student_10_grade = %s, student_10_number = %s, device_id = %s, registration_date = %s, instance_id = %s
-                        """
-            cursor.execute(sql, (
-                form_response["wanum"],
-                form_response["name"],
-                form_response["gender"],
-                form_response["age"],
-                form_response["phnum"],
-                form_response["school"],
-                form_response["optstuno"],
-                form_response["stuname1"],
-                form_response["grade1"],
-                form_response["stumno1"],
-                form_response["stuname2"],
-                form_response["grade2"],
-                form_response["stumno2"],
-                form_response["stuname3"],
-                form_response["grade3"],
-                form_response["stumno3"],
-                form_response["stuname4"],
-                form_response["grade4"],
-                form_response["stumno4"],
-                form_response["stuname5"],
-                form_response["grade5"],
-                form_response["stumno5"],
-                form_response["stuname6"],
-                form_response["grade6"],
-                form_response["stumno6"],
-                form_response["stuname7"],
-                form_response["grade7"],
-                form_response["stumno7"],
-                form_response["stuname8"],
-                form_response["grade8"],
-                form_response["stumno8"],
-                form_response["stuname9"],
-                form_response["grade9"],
-                form_response["stumno9"],
-                form_response["stuname10"],
-                form_response["grade10"],
-                form_response["stumno10"],
-                form_response["deviceid"],
-                form_response["today"],
-                form_response["instanceID"],
-                form_response["name"],
-                form_response["gender"],
-                form_response["age"],
-                form_response["phnum"],
-                form_response["school"],
-                form_response["optstuno"],
-                form_response["stuname1"],
-                form_response["grade1"],
-                form_response["stumno1"],
-                form_response["stuname2"],
-                form_response["grade2"],
-                form_response["stumno2"],
-                form_response["stuname3"],
-                form_response["grade3"],
-                form_response["stumno3"],
-                form_response["stuname4"],
-                form_response["grade4"],
-                form_response["stumno4"],
-                form_response["stuname5"],
-                form_response["grade5"],
-                form_response["stumno5"],
-                form_response["stuname6"],
-                form_response["grade6"],
-                form_response["stumno6"],
-                form_response["stuname7"],
-                form_response["grade7"],
-                form_response["stumno7"],
-                form_response["stuname8"],
-                form_response["grade8"],
-                form_response["stumno8"],
-                form_response["stuname9"],
-                form_response["grade9"],
-                form_response["stumno9"],
-                form_response["stuname10"],
-                form_response["grade10"],
-                form_response["stumno10"],
-                form_response["deviceid"],
-                form_response["today"],
-                form_response["instanceID"]))
-
-            # Store data in Normalized table
-            normalize_table_sql = """INSERT INTO prerna_saathi_final_normalized
-                        (school_udise_code, student_name, student_grade, student_number) VALUES (%s, %s, %s, %s), (%s, %s, %s, %s), (%s, %s, %s, %s), (%s, %s, %s, %s), (%s, %s, %s, %s), (%s, %s, %s, %s), (%s, %s, %s, %s), (%s, %s, %s, %s), (%s, %s, %s, %s), (%s, %s, %s, %s)
-                        """
-            cursor.execute(normalize_table_sql, (
-                form_response["school"],
-                form_response["stuname1"],
-                form_response["grade1"],
-                form_response["stumno1"],
-                form_response["school"],
-                form_response["stuname2"],
-                form_response["grade2"],
-                form_response["stumno2"],
-                form_response["school"],
-                form_response["stuname3"],
-                form_response["grade3"],
-                form_response["stumno3"],
-                form_response["school"],
-                form_response["stuname4"],
-                form_response["grade4"],
-                form_response["stumno4"],
-                form_response["school"],
-                form_response["stuname5"],
-                form_response["grade5"],
-                form_response["stumno5"],
-                form_response["school"],
-                form_response["stuname6"],
-                form_response["grade6"],
-                form_response["stumno6"],
-                form_response["school"],
-                form_response["stuname7"],
-                form_response["grade7"],
-                form_response["stumno7"],
-                form_response["school"],
-                form_response["stuname8"],
-                form_response["grade8"],
-                form_response["stumno8"],
-                form_response["school"],
-                form_response["stuname9"],
-                form_response["grade9"],
-                form_response["stumno9"],
-                form_response["school"],
-                form_response["stuname10"],
-                form_response["grade10"],
-                form_response["stumno10"]))
-        connection.commit()
-    except (Exception, psycopg2.Error) as error:
-        api.logger.error(f"Error in update operation {error}")
-    finally:
-        # closing database connection.
-        if connection:
-            cursor.close()
-            connection.close()
-            api.logger.info("PostgreSQL connection is closed")
-
-
-@celery.task
-def add_task_pathshala(data):
-    try:
-        connection = get_connection()
-        cursor = connection.cursor()
-        for form_response in data:
-            form_response = form_response[0]
-            if not form_response["today"]:
-                form_response["today"] = str((datetime.now()).date())
-            # Store data in main dump table
-            deviceid = form_response["deviceid"]
-            phoneno = form_response["phnum"]
-            udise = form_response["school"]
-            stuname = form_response["name"]
-            grade = form_response["grade"]
-            quizno = form_response["quiz_value"]
-            subject = "Hindi"
-            quizstatus = "C"
-            if grade == "3":
-                q1 = form_response["ques_3_hindi_1_ans"]
-                q2 = form_response["ques_3_hindi_2_ans"]
-                q3 = form_response["ques_3_hindi_3_ans"]
-                q4 = form_response["ques_3_hindi_4_ans"]
-                q5 = form_response["ques_3_hindi_5_ans"]
-                q6 = form_response["ques_3_maths_1_ans"]
-                q7 = form_response["ques_3_maths_2_ans"]
-                q8 = form_response["ques_3_maths_3_ans"]
-                q9 = form_response["ques_3_maths_4_ans"]
-                q10 = form_response["ques_3_maths_5_ans"]
-            elif grade == "4":
-                q1 = form_response["ques_4_hindi_1_ans"]
-                q2 = form_response["ques_4_hindi_2_ans"]
-                q3 = form_response["ques_4_hindi_3_ans"]
-                q4 = form_response["ques_4_hindi_4_ans"]
-                q5 = form_response["ques_4_hindi_5_ans"]
-                q6 = form_response["ques_4_maths_1_ans"]
-                q7 = form_response["ques_4_maths_2_ans"]
-                q8 = form_response["ques_4_maths_3_ans"]
-                q9 = form_response["ques_4_maths_4_ans"]
-                q10 = form_response["ques_4_maths_5_ans"]
-            elif grade == "5":
-                q1 = form_response["ques_5_hindi_1_ans"]
-                q2 = form_response["ques_5_hindi_2_ans"]
-                q3 = form_response["ques_5_hindi_3_ans"]
-                q4 = form_response["ques_5_hindi_4_ans"]
-                q5 = form_response["ques_5_hindi_5_ans"]
-                q6 = form_response["ques_5_maths_1_ans"]
-                q7 = form_response["ques_5_maths_2_ans"]
-                q8 = form_response["ques_5_maths_3_ans"]
-                q9 = form_response["ques_5_maths_4_ans"]
-                q10 = form_response["ques_5_maths_5_ans"]
-            else:
-                q1 = q2 = q3 = q4 = q5 = q6 = q7 = q8 = q9 = q10 = None
-            totmarks = form_response["total_score"]
-            maxmarks = 10
-            instance_id = form_response["instanceID"]
-
-            sql = """INSERT INTO epathshala_quiz_responses1
-                    (deviceid, phoneno, udise, stuname, grade, quizno, subject, quizstatus, q1, q2, q3,  q4, q5, q6,  q7, q8,  q9, q10, totmarks, maxmarks, instance_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-            cursor.execute(sql, (
-                deviceid,
-                phoneno,
-                udise,
-                stuname,
-                grade,
-                quizno,
-                subject,
-                quizstatus,
-                q1,
-                q2,
-                q3,
-                q4,
-                q5,
-                q6,
-                q7,
-                q8,
-                q9,
-                q10,
-                totmarks,
-                maxmarks,
-                instance_id))
+        # Store data in main dump table
+        cursor.execute(query, get_tuple_from_dict(data, param_list))
         connection.commit()
     except (Exception, psycopg2.Error) as error:
         api.logger.error(f"Error in update operation {error}")
@@ -326,7 +105,13 @@ def save_volunteer():
         api.logger.error(f'No data in the input payload')
         return "error"
     else:
-        add_task_volunteer.delay(request_json["data"])
+        for form_response in request_json["data"]:
+            form_response = form_response[0]
+            if not form_response['today']:
+                form_response['today'] = str((datetime.now()).date())
+            normalised_dict = build_dict_epathshala(form_response)
+            add_task.delay(form_response, INSERT_VOLUNTEER, VOLUNTEER_DICT_PARAMS)
+            add_task.delay(normalised_dict, INSERT_VOLUNTEER_NORMALISED, VOLUNTEER_NORMALISED_DICT_PARAMS)
     api.logger.info(f"Received request successfully")
     return "success"
 
@@ -343,21 +128,13 @@ def save_e_pathshala_quiz_data():
         api.logger.error(f'No data in the input payload')
         return "error"
     else:
-        add_task_pathshala.delay(request_json["data"])
+        for form_response in request_json["data"]:
+            form_response = form_response[0]
+            if not form_response['today']:
+                form_response['today'] = str((datetime.now()).date())
+            add_task.delay(form_response, INSERT_EPATHSHALA, EPATHSHALA_DICT_PARAMS)
     api.logger.info(f"Quiz form Submission received request successfully")
     return "success"
-
-
-
-@celery.task
-def add_together(a, b):
-    return a + b
-
-
-@api.route('/add', methods=['GET'])
-def add_route():
-    add_together.delay(34, 55)
-    return "OK"
 
 
 if __name__ == '__main__':
---
+++ utils.py
@@ -0,0 +1,56 @@
+def get_tuple_from_dict(d, param_list):
+    result = ()
+    for param in param_list:
+        result = result + (d[param],)
+    return result
+
+
+def build_dict_epathshala(d):
+    new_dict = {}
+    new_dict["deviceid"] = d["deviceid"]
+    new_dict["phoneno"] = d["phnum"]
+    new_dict["udise"] = d["school"]
+    new_dict["stuname"] = d["name"]
+    new_dict["grade"] = d["grade"]
+    new_dict["quizno"] = d["quiz_value"]
+    new_dict["subject"] = "Hindi"
+    new_dict["quizstatus"] = "C"
+    if new_dict["grade"] == "3":
+        new_dict["q1"] = d["ques_3_hindi_1_ans"]
+        new_dict["q2"] = d["ques_3_hindi_2_ans"]
+        new_dict["q3"] = d["ques_3_hindi_3_ans"]
+        new_dict["q4"] = d["ques_3_hindi_4_ans"]
+        new_dict["q5"] = d["ques_3_hindi_5_ans"]
+        new_dict["q6"] = d["ques_3_maths_1_ans"]
+        new_dict["q7"] = d["ques_3_maths_2_ans"]
+        new_dict["q8"] = d["ques_3_maths_3_ans"]
+        new_dict["q9"] = d["ques_3_maths_4_ans"]
+        new_dict["q10"] = d["ques_3_maths_5_ans"]
+    elif new_dict["grade"] == "4":
+        new_dict["q1"] = d["ques_4_hindi_1_ans"]
+        new_dict["q2"] = d["ques_4_hindi_2_ans"]
+        new_dict["q3"] = d["ques_4_hindi_3_ans"]
+        new_dict["q4"] = d["ques_4_hindi_4_ans"]
+        new_dict["q5"] = d["ques_4_hindi_5_ans"]
+        new_dict["q6"] = d["ques_4_maths_1_ans"]
+        new_dict["q7"] = d["ques_4_maths_2_ans"]
+        new_dict["q8"] = d["ques_4_maths_3_ans"]
+        new_dict["q9"] = d["ques_4_maths_4_ans"]
+        new_dict["q10"] = d["ques_4_maths_5_ans"]
+    elif new_dict["grade"] == "5":
+        new_dict["q1"] = d["ques_5_hindi_1_ans"]
+        new_dict["q2"] = d["ques_5_hindi_2_ans"]
+        new_dict["q3"] = d["ques_5_hindi_3_ans"]
+        new_dict["q4"] = d["ques_5_hindi_4_ans"]
+        new_dict["q5"] = d["ques_5_hindi_5_ans"]
+        new_dict["q6"] = d["ques_5_maths_1_ans"]
+        new_dict["q7"] = d["ques_5_maths_2_ans"]
+        new_dict["q8"] = d["ques_5_maths_3_ans"]
+        new_dict["q9"] = d["ques_5_maths_4_ans"]
+        new_dict["q10"] = d["ques_5_maths_5_ans"]
+    else:
+        new_dict["q1"] = new_dict["q2"] = new_dict["q3"] = new_dict["q4"] = new_dict["q5"] = new_dict["q6"] = new_dict["q7"] = new_dict["q8"] = new_dict["q9"] = new_dict["q10"] = None
+    new_dict["totmarks"] = d["total_score"]
+    new_dict["maxmarks"] = 10
+    new_dict["instance_id"] = d["instanceID"]
+    return new_dict
